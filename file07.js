56959455cf0c0528efbf78a7a4d4d8c7075d5848
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-13 23:14:10 +0530

    Add files via upload

---
+++ file07.js
@@ -0,0 +1,40 @@
+// function greet() {
+//     console.log("Hello World")
+// }
+// greet()
+
+// function greet(name) {
+//     console.log("Hello World " + name)
+// }
+// greet("John")
+// greet("Cathy")
+
+// function greet(name,place) {
+//     console.log("My name is " + name + ". I am from "+ place)
+// }
+// greet("John","Florida")
+// greet("Cathy","States")
+
+
+
+// function greet(name,place) {
+//     console.log(`My name is ${name}. I am from + ${place}.`)
+// }
+// greet("John","Florida")
+// greet("Cathy","States")
+
+
+
+
+// Write a function to accept two numbers and print sum 
+
+// function add(a,b) {
+//     console.log(a+b)
+// }
+// add(4,5)
+
+function add(a,b) {
+    return(a+b)
+}
+let result = add(4,5)
+console.log(result)

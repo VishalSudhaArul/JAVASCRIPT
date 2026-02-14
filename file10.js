d2c75500dc175fdd182a80f6bd84b79c96fed8b6
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-14 21:35:14 +0530

    Add files via upload

---
+++ file10.js
@@ -0,0 +1,18 @@
+// let a=10
+// console.log(a)
+
+// let a = function greet() {
+//     console.log("Hello World");
+// };
+// //console.log(a)
+// a()
+
+// let greet = () => {
+//     console.log("Hello World")
+// };
+// greet()
+
+let add =(x,y) => {
+    console.log(x+y);
+};
+add(4,5);

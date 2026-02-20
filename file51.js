f1b2805421cada2e117f4b20301d4fbcc07cc12d
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-20 18:13:18 +0530

    Add files via upload

---
+++ file51.js
@@ -0,0 +1,8 @@
+//const url = "https://jsonplaceholder.typicode.com/users"
+//const url = "http://localhost:8080/"
+async function fetchData(){
+    const response = await fetch(url)
+    const result = await response.json()
+    console.log(result);
+}
+fetchData() 

06698b33012e31958c55c497ed00d3556ffc59c0
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-20 18:13:52 +0530

    Add files via upload

---
+++ file53.js
@@ -0,0 +1,14 @@
+// closure
+
+function outer() {
+    let n=1;
+    function inner() {
+        return n++;
+    }
+    return inner;
+}
+
+const counter = outer() 
+console.log(counter())
+console.log(counter())
+console.log(counter())

b80e40cd54b1358d839483fd10032ee3959ee900
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-22 14:35:55 +0530

    Add files via upload

---
+++ file58.js
@@ -0,0 +1,22 @@
+// //"use strict"
+//  x = 10
+// console.log(x)
+
+// "use strict"
+// let x = 10 // error
+// console.log(x)
+
+// "use strict"
+// let x = 10 // error
+// console.log(x)
+
+// function add(a,a) {
+//     console.log(a+a)
+// }
+// add(4,5)
+
+
+"use strict"
+function add(a,b) {
+    console.log(a+b)
+}
